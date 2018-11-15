e52a8c7cec9ce7012870d22933310be7bb11da5f
Ayush <ayusha4@illinois.edu>
2018-11-14 18:16:05 -0600

    something

--- build/ece408_src/new-forward.cuh
+++ build/ece408_src/new-forward.cuh
@@ -9,7 +9,9 @@ namespace mxnet
 namespace op
 {
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+#define TILE_WIDTH 8
+
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K, int W_grid)
 {
 
     /*
@@ -21,17 +23,36 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+
+    int b = blockIdx.z;
+    int m = blockIdx.x;
+    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
+    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;
+
+//    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
+//    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
 // y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define y4d(b , m, h, w) y[(b) * (M * H_out * W_out) + (m) * (H_out * W_out) + (h) * (W_out) + w]
+#define x4d(b, c, h_plus_p, w_plus_q) x[(b) * (C * H * W) + (c) * (H * W) + (h_plus_p) * (W) + w_plus_q]
+#define k4d(m, c, p, q) k[(m) * (C * K * K) + (c) * (K * K) + (p) * (K) + q]
 
-    
+float out = 0.0f;
+
+if (b < B && m < M && h < H_out && w < W_out){
+    for (int c = 0; c < C; c++){
+       for (int p = 0; p < K; p++){
+            for (int q = 0; q < K; q++){
+                out += k4d(m, c, p, q) * x4d(b, c, h+p, w+q);
+            }
+        }
+    }    
+}
+
+if(b < B && m < M && h < H_out && w < W_out)
+    y4d(b, m, h, w) = out;
 
 #undef y4d
 #undef x4d
@@ -44,22 +65,41 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
 */
 template <>
-void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
+void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &k)
 {
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+//    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = k.shape_[3];
+    int W_grid = ceil((W - K + 1) / (TILE_WIDTH * 1.0));
+    int H_grid = ceil((H - K + 1) / (TILE_WIDTH *1.0));
+    const int Y = H_grid * W_grid;
+
+/*  printf("LOG : B = %d\n", B);
+    printf("LOG : M = %d\n", M);
+    printf("LOG : C = %d\n", C);
+    printf("LOG : H = %d\n", H);
+    printf("LOG : W = %d\n", W);
+    printf("LOG : K = %d\n", K);
+    printf("LOG : W_grid = %d\n", W_grid);
+    printf("LOG : H_grid = %d\n", H_grid);
+    printf("LOG : Y = %d\n", Y); // 17*17
+*/
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    dim3 gridDim(M, Y, B);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_, x.dptr_, k.dptr_, B, M, C, H, W, K, W_grid);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
--- build/ece408_src/new-forward.h
+++ build/ece408_src/new-forward.h
@@ -21,21 +21,30 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     */
 
     const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = k.shape_[3];
+    int b,m,h,w,c,p,q;
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
 
-    for (int b = 0; b < B; ++b) {
-
-        CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
-
-        /* ... a bunch of nested loops later...
-            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
+    for(b = 0; b < B; b++) {// for each sample in the mini-batch
+        for(m = 0; m < M; m++) { // for each output feature maps
+            for(h = 0; h < H_out; h++) {// for each output element
+                for(w = 0; w < W_out; w++) {
+                    y[b][m][h][w] = 0;
+                    for (c = 0; c < C; c++) {// sum over all input feature maps
+                        for (p = 0; p < K; p++) {// KxK filter
+                            for (q = 0; q < K; q++)
+                                y[b][m][h][w] = y[b][m][h][w] + x[b][c][h+p][w+q] * k[m][c][p][q];
+                        }
+                    }
+		}
+            }
+	}
     }
-
 }
 }
 }
